5693762e00feb3d330382c106a5106cb2c7ef71e
Abhishek220801 <90861021+Abhishek220801@users.noreply.github.com>
2023-05-01 01:52:37 +0530

    Delete sample.java

--- sample.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class sample {
-    public static void main(String[] args) {
-        int x;
-        System.out.println(x);
-        x = 40;
-        // System.out.println(x);
-    }
-}
